f6ff521b4aa486e8dde428e23fc082ce4c0b1782
Huginn87 <32538884+Huginn87@users.noreply.github.com>
2019-04-23 00:17:35 +0200

    Aggiunta mappa labirinto hand made
    Aggiunta mappa labirinto fatta a mano e qualche script per provare il funzionamento del labirinto.

---
+++ ProgettoGaming/Assets/Script/PlayerControllerTestScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControllerTestScript : MonoBehaviour
+{
+    [SerializeField] protected float speed;
+    private Rigidbody rb;
+    private int score;
+    private bool deActivation = false;
+    [SerializeField] protected Text scoreText;
+    [SerializeField] protected Text winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(deActivation);
+            score++;
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Punteggio: " + score.ToString();
+        if(score >= 10)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
---
+++ ProgettoGaming/Assets/Script/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] protected float speedX, speedY, speedZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotationVector3 = new Vector3(speedX, speedY, speedZ);
+        transform.Rotate(rotationVector3 * Time.deltaTime);
+    }
+}

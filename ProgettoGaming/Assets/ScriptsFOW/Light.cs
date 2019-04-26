b81458f865d4b1dfb0cc6a98991171e4189fa969
Huginn87 <32538884+Huginn87@users.noreply.github.com>
2019-04-26 11:08:06 +0200

    Aggiunta luci e nuovo fow plane

---
+++ ProgettoGaming/Assets/ScriptsFOW/Light.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    [SerializeField] protected GameObject lightPlayer;
+    private Vector3 lightOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightOffset = transform.position - lightPlayer.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = lightPlayer.transform.position + lightOffset;
+    }
+}

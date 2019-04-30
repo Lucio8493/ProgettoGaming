a8f26b00a457ffb047d8d1f5c477269043428f03
DESKTOP-4NBMNSH\Lucio <lucio.deluca93@gmail.com>
2019-04-30 12:50:33 +0200

    Aggiunte frecce che puntano verso hunter e prey.

---
+++ ProgettoGaming/Assets/Scripts/Character/ArrowLookAt/Pointing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointing : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject target;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target)
+        {
+            rb.transform.LookAt(target.transform.position);
+        }
+        else
+        {
+            Debug.Log("Target not set");
+        }
+    }
+}

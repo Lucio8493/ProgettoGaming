d1211d6540bd9b2a5cee973b94b694121bf61615
FranTedesco <tedesco.uni@gmail.com>
2019-04-30 09:46:36 +0200

    Aggiunto al topino lo script (creato qui) AnimateCharacter per una prima animazione di camminata

---
+++ ProgettoGaming/Assets/Scripts/Character/AnimateCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character
+{
+    public class AnimateCharacter : MonoBehaviour
+    {
+
+        protected CharacterController _charController;
+        protected CharacterStatus status;
+        Animator anim;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _charController = GetComponent<CharacterController>();
+            status = GetComponent<CharacterStatus>();
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (status.IsMoving)
+            {
+                anim.SetInteger("speed", 1);
+            } else
+            {
+                anim.SetInteger("speed", 0);
+
+            }
+        }
+    }
+}

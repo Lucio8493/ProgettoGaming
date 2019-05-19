33a935bd288d1ba17f8f45b2a341dc4e33c94967
Huginn87 <32538884+Huginn87@users.noreply.github.com>
2019-05-19 09:53:29 +0200

    Menu con eventi v119-05-19

---
+++ ProgettoGaming/Assets/Scripts/MenuScript/GameEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameEvent
+{
+    public const string CHANGE_SCENE = "CHANGE_SCENE";
+    public const string SETTING_PLAYERS_NUMBER = "SETTING_PLAYERS_NUMBER";
+    public const string SPEED_CHANGE = "SPEED_CHANGE";
+    public const string VISIBILITY_RANGE_INCREASED = "VISIBILITY_RANGE_INCREASED";
+    public const string POSITION_OFFSET = "POSITION_OFFSET";
+
+}
+
---
+++ ProgettoGaming/Assets/Scripts/MenuScript/SceneHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        Messenger.AddListener(GameEvent.CHANGE_SCENE, ChangeScene);
+        Messenger.AddListener(GameEvent.SETTING_PLAYERS_NUMBER, SetNumberOfPlayers);
+    }
+
+    private void SetNumberOfPlayers()
+    {
+        SettingsClass.NumOfPlayers = 6;
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}

fd201fb872359d5b30663eeed94a6be962daa4f6
FranTedesco <tedesco.uni@gmail.com>
2019-05-19 13:28:53 +0200

    Creato Oggetto Bonus

---
+++ ProgettoGaming/Assets/Scripts/Bonus/ReadBonuses.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bonus //restituisce una lista di bonus
+{
+    public class ReadBonuses
+    {
+
+    }
+
+}

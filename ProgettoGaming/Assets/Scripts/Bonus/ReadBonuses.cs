fe0742627fc3783b346c4f868ec2300d18112b8d
FranTedesco <tedesco.uni@gmail.com>
2019-05-19 13:47:03 +0200

    Creazione di prima bozza di ReadBonuses

--- ProgettoGaming/Assets/Scripts/Bonus/ReadBonuses.cs
+++ ProgettoGaming/Assets/Scripts/Bonus/ReadBonuses.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 namespace Bonus //restituisce una lista di bonus
 {
+
+
     public class ReadBonuses
     {
+          List<Bonus> bonuses;
+
+
+        public ReadBonuses()
+        {
+             bonuses  = new List<Bonus>();
+
+            Bonus b = new Bonus(1);
+            b.SpeedBoost = 1.5f;
+            b.Seconds = 4f;
+
+            bonuses.Add(b);
+
+        }
+
+
+        public List<Bonus> getBonuses()
+        {
+           return bonuses;
+        }
 
     }
 

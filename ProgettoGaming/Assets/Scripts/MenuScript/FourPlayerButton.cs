d539a62a7e71d82ce8cc2f1099739699c8654b99
Huginn87 <32538884+Huginn87@users.noreply.github.com>
2019-05-20 15:00:11 +0200

    Aggiunto bottone 4 giocatori

---
+++ ProgettoGaming/Assets/Scripts/MenuScript/FourPlayerButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourPlayerButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Messenger<int>.Broadcast(GameEvent.SETTING_PLAYERS_NUMBER, 4);
+    }
+}

f286c4666ae4cc64c532797cb2e817d119fea9b5
FranTedesco <tedesco.uni@gmail.com>
2019-05-29 15:27:33 +0200

    Prima implementazione del bonus UI in gioco che mostra quale bonus hai ottenuto

---
+++ ProgettoGaming/Assets/Bonus/BonusUI.cs
@@ -0,0 +1,40 @@
+﻿using Character;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusUI : MonoBehaviour
+{
+
+    GameObject player = null;
+    Animator anim;
+    protected CharacterStatus status;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tempList = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
+        foreach (GameObject p in tempList)
+        {
+            if (p.GetComponent<Character.CharacterStatus>().MyType == Character.CharacterStatus.typeOfPlayer.Player)
+            {
+                player = p;
+                anim = GetComponent<Animator>();
+                status = p.GetComponent<CharacterStatus>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (status.UsingBonus)
+            anim.SetInteger("BonusId", 1);
+        else
+            anim.SetInteger("BonusId", 0);
+
+
+    }
+}

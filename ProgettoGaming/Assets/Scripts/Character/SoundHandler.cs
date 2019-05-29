34985eaa78337b7587e59dac752aa498b81439c0
DESKTOP-4NBMNSH\Lucio <lucio.deluca93@gmail.com>
2019-05-29 16:20:38 +0200

    Risolto bug tutorialScene2 e prima bozza del gestore audio

---
+++ ProgettoGaming/Assets/Scripts/Character/SoundHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Character;
+
+public class SoundHandler : MonoBehaviour
+{
+    [SerializeField]
+    protected AudioClip[] footsteps;
+    
+
+    private AudioSource audioSource;
+    private CharacterStatus status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+        status = this.GetComponent<CharacterStatus>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (status.IsMoving && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(footsteps[Random.Range(0, footsteps.Length)]);
+        }
+    }
+}
